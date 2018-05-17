967d22db3ab6ad167030ca80f0172d6a8276c917
Ichwan <ipalongengi@gmail.com>
2018-05-17 19:22:37 -0400

    fix some erroneously used RDD objects

--- cluster_script/cities_stats.py
+++ cluster_script/cities_stats.py
@@ -37,15 +37,15 @@ if __name__ == '__main__':
 
     # Parse the businesses rdd
     biz_rdd = sc.textFile(BIZ_FN, use_unicode=False) \
-                .mapPartitionsWithIndex(extract_biz)
+                .mapPartitionsWithIndex(extract_biz) \
+                .cache()
 
 
     def find_stuff(city, biz_rdd, review_rdd):
         from operator import add
 
         # Create a new RDD containing only businesses from the specified city
-        city_biz_rdd = biz_rdd.filter(lambda x: x[4] == city) \
-                              .cache()
+        city_biz_rdd = biz_rdd.filter(lambda x: x[4] == city)
 
         ## What are the most common business categories in this city?
         top_biz_cat = city_biz_rdd.map(lambda x: (x[0], x[12].split(';'))) \
@@ -66,7 +66,7 @@ if __name__ == '__main__':
                                       .map(lambda x: (x[1][0], x[1][1])) \
                                       .sortBy(lambda x: x[1], ascending=False)
 
-        biz_rat_total.saveAsTextFile('%s_biz_review_total' % city)
+        biz_rat_total.saveAsTextFile('%s_top_biz_review_total' % city)
 
         ## What are the business establishments in this city with the most positive reviews,
         ## regardless of their business categories?
@@ -76,8 +76,8 @@ if __name__ == '__main__':
                                  .reduceByKey(add) \
                                  .sortBy(lambda x: x[1][1], ascending=False)
 
-        city_biz_top = biz_rdd.join(biz_rat_top) \
-                              .sortBy(lambda x: x[1][1], ascendind=False) \
+        city_biz_top = city_biz_rdd.join(biz_rat_top) \
+                              .sortBy(lambda x: x[1][1], ascending=False) \
                               .map(lambda x: x[1])
 
         city_biz_top.saveAsTextFile('%s_biz_top_rating' % city)
@@ -85,13 +85,13 @@ if __name__ == '__main__':
         ## What are the business establishments in this city with the most negative reviews,
         ## regardless of their business categories?
         biz_rat_bottom = review_rdd.map(lambda x: (x[2], int(x[3]))) \
-                                 .filter(lambda x: x[1] >= 3) \
+                                 .filter(lambda x: x[1] < 3) \
                                  .map(lambda x: (x[0], 1)) \
                                  .reduceByKey(add) \
                                  .sortBy(lambda x: x[1][1], ascending=False)
 
-        city_biz_bottom = biz_rdd.join(biz_rat_bottom) \
-                                .sortBy(lambda x: x[1][1], ascendind=False) \
+        city_biz_bottom = city_biz_rdd.join(biz_rat_bottom) \
+                                .sortBy(lambda x: x[1][1], ascending=False) \
                                 .map(lambda x: x[1])
 
         city_biz_bottom.saveAsTextFile('%s_biz_bottom_rating' % city)
